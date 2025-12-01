6ce4d7659b7002165c211dbb09ce43fa5f07bb98
Mahmut Can Conger <mahmutconger@gmail.com>
2025-12-02 00:38:36 +0300

    Refactor App component structure and remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from "./components/Navbar";
+import Hero from "./sections/Hero";
+import About from "./sections/About"; // 1. İçe Aktar
+import TechStack from "./sections/TechStack";
+import Projects from "./sections/Projects";
+import Footer from "./sections/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="bg-slate-900 min-h-screen text-white selection:bg-blue-500 selection:text-white">
+      <Navbar />
+      
+      <main>
+        <Hero />
+        <About />     {/* 2. Hero'dan hemen sonra buraya ekledik */}
+        <TechStack />
+        <Projects />
+      </main>
+
+      <Footer />
+    </div>
   )
 }
 
