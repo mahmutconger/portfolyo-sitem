fa92b4bd0c8a12f0cd9fa76853df173cc3f87efa
Mahmut Can Conger <mahmutconger@gmail.com>
2025-12-02 14:50:36 +0300

    Add Home page with Navbar, Hero, About, TechStack, Projects, and Footer components

---
+++ src/pages/Home.tsx
@@ -0,0 +1,26 @@
+import Navbar from "../components/Navbar";
+import Hero from "../sections/Hero";
+import About from "../sections/About"; // 1. İçe Aktar
+import TechStack from "../sections/TechStack";
+import Projects from "../sections/Projects";
+import Footer from "../sections/Footer";
+
+
+function Home() {
+  return (
+    <div className="bg-slate-900 min-h-screen text-white selection:bg-blue-500 selection:text-white">
+      <Navbar />
+      
+      <main>
+        <Hero />
+        <About />     {/* 2. Hero'dan hemen sonra buraya ekledik */}
+        <TechStack />
+        <Projects />
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
