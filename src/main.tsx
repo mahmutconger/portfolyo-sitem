516a9cbab44c082b35cb4f22bf45eb1667da362b
Mahmut Can Conger <mahmutconger@gmail.com>
2025-12-02 14:50:10 +0300

    Wrap App component with BrowserRouter for routing support

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom' // 1. Bunu ekleyin
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    {/* 2. App'i BrowserRouter ile sarmalayın */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 )
