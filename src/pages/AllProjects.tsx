5599b844b50f62258f633cdf800e3918a3fc9158
Mahmut Can Conger <mahmutconger@gmail.com>
2025-12-02 14:50:31 +0300

    Add AllProjects page with Navbar, Projects, and Footer components

---
+++ src/pages/AllProjects.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../sections/Footer';
+import Projects from '../sections/Projects';
+
+const AllProjects = () => {
+  return (
+    <div className="bg-slate-900 min-h-screen">
+      <Navbar /> {/* Menü her sayfada olsun */}
+      
+      {/* isFullList=true diyerek filtrelemeden hepsini istiyoruz */}
+      <Projects isFullList={true} />
+      
+      <Footer />
+    </div>
+  );
+};
+
+export default AllProjects;
